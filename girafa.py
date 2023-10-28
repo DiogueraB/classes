e3d033c3553ee078579e7eb1b1a3325840a70601
DiogueraB <diogobatistaguimaraes10@gmail.com>
2023-10-28 16:50:32 -0300

    1 Commit

---
+++ girafa.py
@@ -0,0 +1,66 @@
+import time
+from random import choice
+
+# Define que estou criando uma classe com nome "Girafa"
+# Começa sempre com a palavra maiúscula.
+class Girafa: 
+    # Propriedades
+    
+    # O método __Init__ é é executado quando 
+    # a classe é instanciada
+    def __init__(self, nome, altura, cor, idade, origem): 
+        self.nome = nome
+        self.altura = altura
+        self.idade = idade
+        self.cor = cor
+        self.origem = origem
+        self.__fome = 100
+
+    def andar(self):
+        if self.__fome > 60:
+            print(self.nome, "não pode andar pois está morrendo de fome!!")
+        else:
+            print(self.nome, "está andando....")
+
+
+    def comer(self, alimento):
+        lista_alimentos = ['folhas', 'frutas', 'plantas']
+        
+        if alimento in lista_alimentos:
+            self.__fome -= 10
+        else:
+            print(self.nome, "não come esse tipo de coisa....")
+
+    def fome(self):
+        if self.__fome > 60:
+            print(self.nome, "está morrendo de fome!")
+        elif self.__fome >  40:
+            print(self.nome, "está com fome!")
+        elif self.__fome > 20:
+            print(self.nome, "está saciada!")
+        elif self.__fome > 0:
+            print(self.nome, "está cheia!")
+        elif self.__fome <= 0:
+            print(self.nome, "está explodindo!!")
+
+    def respirar(self):
+        print(self.nome, "Inspira...")
+        time.sleep(2)
+        print(self.nome, "Expira...")
+
+    # Função para reproduzir girafas, ela recebe um parceiro
+    # e retorna um filhote
+    def reproduzir(self, parceiro):
+        if not isinstance(parceiro, Girafa):
+            print(self.nome, "sai correndo desesperado")
+
+        nome = "Filhote de ", self.nome
+        altura = (self.altura + parceiro.altura) / 2
+        idade = 1
+        cor = choice([self, parceiro]).cor
+        origem = self.origem
+
+
+        filhote = Girafa(nome, altura, idade, cor, origem)
+
+        return filhote
---
+++ main.py
@@ -0,0 +1,13 @@
+from girafa import Girafa # Do arquivo girafa importa Girafa
+
+girafa = Girafa("Melman", 4.15, "Amarela e Marrom", 32, "Nova York") 
+girafa2 = Girafa("Gloria", 5, "Amarela e Marrom", 30, "Africa Central")
+
+print(girafa.nome)
+girafa2.comer('folhas')
+girafa2.comer('folhas')
+
+girafa2.fome()
+girafa2.andar()
+girafa2.respirar()
+
